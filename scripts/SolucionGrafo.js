c061bbbc0b5c0fef820196bd60cf102d0cda4968
Abraham <aromeroa1500@alumno.ipn.mx>
2022-06-04 22:58:35 -0500

    Cometarios

---
+++ scripts/SolucionGrafo.js
@@ -0,0 +1,47 @@
+/*Proyecto final analisis de algoritmos*/
+
+/*Implementacion del algortimo PRIM para optimizar un rafo que represente 
+las diferentes tareas del hogar que se realizan en el dia a dia */
+
+/*https://correoipn-my.sharepoint.com/:w:/g/personal/aromeroa1500_alumno_ipn_mx/ESMd2bBdaDRAgzfYQTfkglcBaUFXCg2e7BjFvENSmgxeDA?e=DEg5ZW*/
+/*
+    Lista de tareas:  
+     1. Tender las camas - A
+     2. Lavar la ropa - B
+     3. Lavar el piso - C
+     4. Lavar los trates - D
+     5. Bañar al perro - E
+     6. Limpiar la lavadora -F
+     7. Doblar la ropa - G
+     8. Tender la ropa - H
+     9. Sacar la basura - I 
+     10. Lavar el patio - J
+     11. EF - K
+     12 FEW - L
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
